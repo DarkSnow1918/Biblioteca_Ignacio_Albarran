6d90826c58c739f0e6a06a6301f578969069df42
ShonaDarkSnow1918 <nashoalbarran19@gmail.com>
2020-10-11 08:21:49 -0300

    añadí dos libros más a mi spin Modifiqué los valores de los libros saqué el botón

--- app/src/main/java/com/example/bibliotecadigital/GitHub_act.java
+++ app/src/main/java/com/example/bibliotecadigital/GitHub_act.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,10 +24,10 @@ public class GitHub_act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git_hub_act);
 
-        spin =(Spinner) findViewById(R.id.SpLibros);
-        texto = (TextView)findViewById(R.id.TvMostrar);
+        spin = (Spinner) findViewById(R.id.SpLibros);
+        texto = (TextView) findViewById(R.id.TvMostrar);
 
-        ArrayList<String>listaLibros = (ArrayList<String>) getIntent().getSerializableExtra("listaLibros");
+        ArrayList<String> listaLibros = (ArrayList<String>) getIntent().getSerializableExtra("listaLibros");
         listaLibros.add(libro.getFarenheit());
         listaLibros.add(libro.getRevival());
         listaLibros.add(libro.getAlquimista());
@@ -36,34 +37,35 @@ public class GitHub_act extends AppCompatActivity {
         ArrayAdapter<String> adaptLibros = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaLibros);
 
         spin.setAdapter(adaptLibros);
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-    }
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (spin.getSelectedItem().equals(libro.getFarenheit())) {
+                    texto.setText("El valor de Farenheit es: " + libro.getPreFarenheit());
+                }
 
-    public void Mostrar(View view)
-    {
-        if(spin.getSelectedItem().equals(libro.getFarenheit()))
-        {
-            texto.setText("El valor de Farenheit es: "+ libro.getPreFarenheit());
-        }
+                if (spin.getSelectedItem().equals(libro.getRevival())) {
+                    texto.setText("El valor de Revival es: " + libro.getPreRevival());
+                }
 
-        if(spin.getSelectedItem().equals(libro.getRevival()))
-        {
-            texto.setText("El valor de Revival es: "+ libro.getPreRevival());
-        }
+                if (spin.getSelectedItem().equals(libro.getAlquimista())) {
+                    texto.setText("El valor de El Alquimista es: " + libro.getPreElAlquimista());
+                }
 
-        if(spin.getSelectedItem().equals(libro.getAlquimista()))
-        {
-            texto.setText("El valor de El Alquimista es: "+ libro.getPreElAlquimista());
-        }
+                if (spin.getSelectedItem().equals(libro.getPoder())) {
+                    texto.setText("El valor de El Poder es: " + libro.getPrePoder());
+                }
+                if (spin.getSelectedItem().equals(libro.getDespertar())) {
+                    texto.setText("El valor de Despertar es: " + libro.getPreDespertar());
+                }
 
-        if(spin.getSelectedItem().equals(libro.getPoder()))
-        {
-            texto.setText("El valor de El Poder es: "+libro.getPrePoder());
-        }
-        if(spin.getSelectedItem().equals(libro.getDespertar()))
-        {
-            texto.setText("El valor de Despertar es: "+libro.getPreDespertar());
-        }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
